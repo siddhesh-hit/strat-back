b3fb2e9f8b5dc7c9b051bef4579fdbe883d3c408
siddhesh-hit <siddhesh@handsintechnology.com>
2023-10-09 14:33:38 +0530

    databse config

--- config.js
+++ config.js
@@ -1,11 +1,19 @@
 const mysql = require("mysql");
 
-const conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "db_strategytool",
-  // database: "handsin_strategytool",
-});
+// const conn = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "db_strategytool",
+//   // database: "handsin_strategytool",
+// });
 
+const conn = mysql.createConnection({
+  host: env("DATABASE_HOST", "localhost"),
+  port: env.int("DATABASE_PORT", 3306),
+  database: env("DATABASE_NAME", "strategytool"),
+  user: env("DATABASE_USERNAME", "strategytooluser"),
+  password: env("DATABASE_PASSWORD", "Ekk#hy&2e%~y"),
+  ssl: env.bool("DATABASE_SSL", false),
+});
 module.exports = conn; // Export the connection object
